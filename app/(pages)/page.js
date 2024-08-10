4f6ce72f1fe2423bd22386259b987ceca41f23d3
Fateme Hasanzade <hasanzade.f5716@gmail.com>
2024-08-10 18:24:15 +0330

    create pages folder and move page file in pages folder

---
+++ app/(pages)/page.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import "../globals.css";
+export default function Home() {
+  return (
+    <>
+      <Typography className="sky_blue" fontSize={"2rem"}>
+        hello world سلام جوجو فونت یاب
+      </Typography>
+      <Typography fontSize={"2rem"}>سلام جوجو</Typography>
+    </>
+  );
+}
